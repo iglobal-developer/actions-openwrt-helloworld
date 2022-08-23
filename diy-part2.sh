5826aa9524e2bbb9f2cda66136710dad0e2d706f
iGlobal Dev <develop.iglobal@gmail.com>
2022-08-23 09:34:05 +0000

    Fixed including custom files

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,7 +10,6 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.1.11/g' package/base-files/files/bin/config_generate
-cp -rf "$GITHUB_WORKSPACE/files/roots/" package/base-files/files/
 rm -rf package/lean/luci-theme-argon
 git clone https://github.com/iglobal-developer/luci-theme-argon.git package/lean/luci-theme-argon
 rm -rf package/luci
