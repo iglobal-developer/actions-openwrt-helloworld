f77e56b395936d9171ce76b68b4b081f5b920cb6
iGlobal Dev <develop.iglobal@gmail.com>
2022-10-06 09:26:26 +0000

    removed footprint

--- diy-part2.sh
+++ diy-part2.sh
@@ -8,6 +8,17 @@
 # Blog: https://p3terx.com
 #============================================================
 
+# Add the default password for the 'root' user（Change the empty password to 'password'）
+sed -i 's/root:::0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/g' package/base-files/files/etc/shadow
+sed -i 's/nobody:\*:0:0:99999:7:::/nobody:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/g' package/base-files/files/etc/shadow
+
+grep -v "DISTRIB_DESCRIPTION=" package/base-files/files/etc/openwrt_release > tmpFile && mv tmpFile package/base-files/files/etc/openwrt_release
+echo "DISTRIB_DESCRIPTION='VSocks V1.0'" >>package/base-files/files/etc/openwrt_release
+grep -v "luciname    = " package/base-files/files/usr/lib/lua/luci/version.lua  > tmpFile  && mv tmpFile package/base-files/files/usr/lib/lua/luci/version.lua
+echo "luciname    = VPROXY" >> package/base-files/files/usr/lib/lua/luci/version.lua
+grep -v "luciversion = " package/base-files/files/usr/lib/lua/luci/version.lua  > tmpFile  && mv tmpFile package/base-files/files/usr/lib/lua/luci/version.lua
+echo "luciversion = V1.0" >> package/base-files/files/usr/lib/lua/luci/version.lua
+
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.1.11/g' package/base-files/files/bin/config_generate
 rm -rf package/lean/luci-theme-argon
